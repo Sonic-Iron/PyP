e371fa2063000333adb26039970649caa68ff9e9
David Juckes <david.a.juckes@tesco.net>
2016-11-05 21:50:36 +0000

    Cleaned up code so it now is ready for more compression types
    ??? It hasn't been committed in previous 'commits'?

--- PyP.py
+++ PyP.py
@@ -2,10 +2,8 @@ global root, progress, progc, compress, target, extensions, confframe, comptypes
 
 import os, sys, time, threading
 
-target = 'To compress'
-compto = 'C:/Users/David/Documents/GitHub/PyP/compression.txt'
-extensions = ['py', 'png', 'txt']
-comp_types = ['PyP', 'xd'] #Default is first
+extensions = ['py', 'txt', 'bat']
+comp_types = ['PyP', 'Python'] #Default is first
 print('PyP')
 
 import tkinter as tk
@@ -35,9 +33,13 @@ def start_compression():
     compress = True
 
 def compression():
-    global confframe
     while not compress:
         pass
+    global confframe, toentry, locentry, target, compto
+    target = locentry.get()
+    compto = toentry.get()
+    if target[len(target)-1] != '/':
+        target = target + '/'
     steps = 17
     folders = []
     files = []
@@ -51,45 +53,52 @@ def compression():
         confframe.destroy()
     except tk.TclError:
         pass
-    def scan(self, d):
-        global items
-        for thing in os.listdir(d):
-            if not thing == d[0]:
-                try:
-                    scan(self, d + '/' + thing)
-                    folders.append(d + '/' + thing)
-                except OSError:
-                    catch = False
-                    for extension in extensions:
-                        if thing[len(thing)-len(extension):] == extension:
-                            catch = True
-                    if catch:
-                        files.append(d + '/' + thing)
-    scan(scan, target)
-    total = 0
-    step = 85 / (len(folders) + len(files))
-    pos = 1
-    file = open(compto, 'w')
-    file.write(target + '$\n')
-    file.close()
-    file = open(compto, 'a')
-    for item in folders:
-        file.write(item + '\n')
-        progc.step(progress, step * pos)
-        pos = pos + 1
-    file.write('$\n')
-    for item in files:
-        file.write(item + ':')
-        try:
-            f = open(item, 'r')
-            file.write(f.read() + '\n!divider!\n')
-            f.close()
-        except OSError:
-            file.write('PyP - Error while packing\n')
-            print('Error while writing in ' + item)
-        progc.step(progress, step * pos)
-        pos = pos + 1
-    file.close()
+    if ctype == 'PyP':
+        def scan(self, d):
+            global items
+            for thing in os.listdir(d):
+                if not thing == d[0]:
+                    try:
+                        scan(self, d + '/' + thing)
+                        folders.append(d + '/' + thing)
+                    except OSError:
+                        catch = False
+                        for extension in extensions:
+                            if thing[len(thing)-len(extension):] == extension:
+                                catch = True
+                        if catch:
+                            files.append(d + '/' + thing)
+        scan(scan, target)
+        total = 0
+        step = 85 / (len(folders) + len(files))
+        pos = 1
+        file = open(compto, 'w')
+        file.write(target + '$\n')
+        file.close()
+        file = open(compto, 'a')
+        for item in folders:
+            file.write(item + '\n')
+            progc.step(progress, step * pos)
+            pos = pos + 1
+        file.write('$\n')
+        for item in files:
+            file.write(item + ':')
+            try:
+                f = open(item, 'r')
+                file.write(f.read() + '\n!divider!\n')
+                f.close()
+            except OSError:
+                file.write('PyP - Error while packing\n')
+                print('Error while writing in ' + item)
+            progc.step(progress, step * pos)
+            pos = pos + 1
+        file.close()
+    def showfile():
+        os.popen('"' + sys.path[0] + '/' + compto + '"')
+    donelabel = tk.Label(root, text='Done!')
+    donelabel.pack(side=tk.BOTTOM)
+    donebutton = tk.Button(root, text='Show', command=showfile)
+    donebutton.pack(side=tk.BOTTOM, fill=tk.X)
 
 def start_decompression():
     global decompress
@@ -99,6 +108,9 @@ def decompression():
     global toentry, locentry
     while not decompress:
         pass
+    progress = tk.Canvas(root, width=100, height=50)
+    progc = progresscanvas(progress)
+    progress.pack()
     locentry = locentry.get()
     toentry = toentry.get()
     confframe.destroy()
@@ -109,9 +121,15 @@ def decompression():
     file.close()
     target, folders, files = todo.split('$')
     os.mkdir(toentry + target)
+    step = 85 / (1 + len(folders.split('\n')) + len(files.split('\n!divider!\n')))
+    pos = 1
+    progc.step(progress, step * pos)
+    pos = pos + 1
     for item in folders.split('\n'):
         if not item == '':
             os.mkdir(toentry + item)
+        progc.step(progress, step * pos)
+        pos = pos + 1
     for item in files.split('\n!divider!\n'):
         if not item == '':
             name, contents = item.split(':')
@@ -120,12 +138,24 @@ def decompression():
             file = open(toentry + name, 'w')
             file.write(contents)
             file.close()
+        progc.step(progress, step * pos)
+        pos = pos + 1
+    donelabel = tk.Label(root, text='Done!')
+    donelabel.pack(side=tk.BOTTOM)
 
 def c():
-    global confframe, comptypes
+    global confframe, comptypes, locentry, toentry
     startframe.destroy()
     confframe = tk.Frame(root)
     comptypes = tk.Listbox(confframe)
+    loclabel = tk.Label(confframe, text='Item to pack')
+    locentry = tk.Entry(confframe)
+    tolabel = tk.Label(confframe, text='Pack into')
+    toentry = tk.Entry(confframe)
+    loclabel.pack(anchor='nw')
+    locentry.pack(anchor='nw')
+    tolabel.pack(anchor='nw')
+    toentry.pack(anchor='nw')
     gobutton = tk.Button(confframe, text='Start', command=start_compression)
     for t in comp_types:
         comptypes.insert(tk.END, t)
